e08ba8cab98ad953cdb4ae5ee5437c7c10c3b0f1
Karl <kcayme@gmail.com>
2022-07-05 14:10:40 +0800

    case insensitive sort implemented

--- app/src/main/java/com/example/wordy/MainActivity.kt
+++ app/src/main/java/com/example/wordy/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.wordy
 
 import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.wordy.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 /*
 *Create a wordy app that includes the following:
@@ -17,6 +21,7 @@ import com.example.wordy.databinding.ActivityMainBinding
 * */
 class MainActivity : AppCompatActivity() {
     private var wordList = ArrayList<String>()
+    @RequiresApi(Build.VERSION_CODES.N)
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,18 +41,19 @@ class MainActivity : AppCompatActivity() {
                 val newWord = binding.wordInput.text.toString().trim()
                 wordList.add(newWord)
                 if (binding.radioButtonAsc.isChecked){
-                    wordList.sort()
-                    Toast.makeText(this,"Ascending Selected",Toast.LENGTH_SHORT).show()
+                    Collections.sort(wordList,String.CASE_INSENSITIVE_ORDER)
+                    Toast.makeText(this,"Ascending",Toast.LENGTH_SHORT).show()
                 }
                 else if (binding.radioButtonDesc.isChecked) {
-                    wordList.sortDescending()
+                    Collections.sort(wordList,String.CASE_INSENSITIVE_ORDER.reversed())
                     Toast.makeText(this,"Descending Selected",Toast.LENGTH_SHORT).show()
                 }
                 val textview = binding.wordList
                 textview.text = ""
                 for(word in wordList){
                     val currentText = textview.text.toString()
-                    textview.text = "$currentText\n$word"
+                    val appendStr = "$currentText\n$word"
+                    textview.text = appendStr.trim()
                 }
                 binding.wordInput.setText("")
             }
